04881e60304d19ae63d3e8588afbee7dc67fd32e
rajanvishwa10 <rajanvishwa10@gmail.com>
2021-01-05 22:32:52 +0530

    added notification fragment with shimmer effect

--- app/src/main/java/com/android/socialmedia/RegisterActivity.java
+++ app/src/main/java/com/android/socialmedia/RegisterActivity.java
@@ -3,7 +3,9 @@ package com.android.socialmedia;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +17,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -27,10 +34,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         mAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Registering");
         progressDialog.setMessage("Uploading data...");
+
     }
 
     public void register(View view) {
@@ -53,13 +62,10 @@ public class RegisterActivity extends AppCompatActivity {
         } else if (email.isEmpty()) {
             editText2.setError("Enter Email");
             editText2.requestFocus();
-        }
-        else if (!email.matches(emailPattern))
-        {
+        } else if (!email.matches(emailPattern)) {
             editText2.setError("Enter proper Email");
             editText2.requestFocus();
-        }
-        else if (username.isEmpty()) {
+        } else if (username.isEmpty()) {
             editText3.setError("Enter Username");
             editText3.requestFocus();
         } else if (password.isEmpty() || password.length() < 6) {
@@ -69,8 +75,35 @@ public class RegisterActivity extends AppCompatActivity {
             editText4.setError("Password should match");
             editText4.requestFocus();
         } else {
-            progressDialog.show();
-            addData(name, email, username, password);
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+            Query query = databaseReference.orderByChild("Username").equalTo(username);
+            query.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (!snapshot.exists()) {
+                        progressDialog.show();
+                        addData(name, email, username, password);
+                    }else{
+                        System.out.println("data already exists");
+                        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                        builder.setTitle("Error");
+                        builder.setMessage("Username already Exists");
+                        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        builder.show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
         }
     }
 
@@ -88,7 +121,7 @@ public class RegisterActivity extends AppCompatActivity {
                             hashMap.put("followers", 0);
                             hashMap.put("following", 0);
                             hashMap.put("profileImage", "");
-                            hashMap.put("verified",false);
+                            hashMap.put("verified", false);
                             FirebaseDatabase.getInstance().getReference("Users").
                                     child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                     .setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
