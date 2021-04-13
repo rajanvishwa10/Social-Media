b9404c421c7804360cd49356e3ae2226cf402b87
rajanvishwa10 <rajanvishwa10@gmail.com>
2021-04-13 17:26:59 +0530

    final commit

---
+++ app/src/main/java/com/android/socialmedia/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.android.socialmedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        },2500);
+    }
+}
